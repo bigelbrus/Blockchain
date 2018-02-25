4983fe2473474b34d70b5d7c9b44296ad623c719
bigelbrus <bigelbrus@gmail.com>
2018-02-25 21:57:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import com.google.gson.GsonBuilder;
+
+import java.security.Security;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    public static ArrayList<Block> blockchain = new ArrayList<>();
+    public static HashMap<String,TransactionOutput> UTXOs = new HashMap<>();
+    public static int difficulty = 5;
+    public static float minimumTransaction = 0.1f;
+    public static Wallet walletA;
+    public static Wallet walletB;
+
+    public static void main(String[] args) {
+        //Setup Bouncey castle as a Security Provider
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        //Create the new wallets
+        walletA = new Wallet();
+        walletB = new Wallet();
+        //Test public and private keys
+        System.out.println("Private and public keys:");
+        System.out.println(StringUtil.getStringFromKey(walletA.privateKey));
+        System.out.println(StringUtil.getStringFromKey(walletA.publicKey));
+        //Create a test transaction from WalletA to walletB
+        Transaction transaction = new Transaction(walletA.publicKey, walletB.publicKey, 5, null);
+        transaction.generateSignature(walletA.privateKey);
+        //Verify the signature works and verify it from the public key
+        System.out.println("Is signature verified");
+        System.out.println(transaction.verifiySignature());
+
+    }
+
+    public static Boolean isChainValid(){
+        Block currentBlock;
+        Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0','0');
+
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+            if(!currentBlock.hash.equals(currentBlock.calculateHash())){
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            if(!previousBlock.hash.equals(currentBlock.previousHash)){
+                System.out.println("Previous hashes not equal");
+                return false;
+            }
+            if(!currentBlock.hash.substring(0,difficulty).equals(hashTarget)){
+                System.out.println("This block hasnt been mined");
+                return false;
+            }
+        }
+        return true;
+    }
+}
